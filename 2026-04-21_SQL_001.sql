011d0ca5bfc8c7b1b80f776a525772c54c0180d7
Shashikant Shukla <sks7523830807@gmail.com>
2026-04-21 17:04:14 +0530

    Create 2026-04-21_SQL_001.sql

---
+++ 2026-04-21_SQL_001.sql
@@ -0,0 +1,7 @@
+/* Find all posts which were reacted to with a heart. For such posts output all columns from facebook_posts table.*/
+
+select distinct fp.* from facebook_posts fp
+inner join facebook_reactions as fr
+on fp.post_id = fr.post_id and
+fr.reaction ='heart'
+ 
