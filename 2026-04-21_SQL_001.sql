38461095b1a5a445c7584c786f78bf2a1bc08a99
Shashikant Shukla <sks7523830807@gmail.com>
2026-04-22 16:14:47 +0530

    Fix company name and update comment for SQL query

--- 2026-04-21_SQL_001.sql
+++ 2026-04-21_SQL_001.sql
@@ -1,5 +1,6 @@
-# Compan_name : Meta
-/* Find all posts which were reacted to with a heart. For such posts output all columns from facebook_posts table.*/
+# Company_name : Meta
+/* Question : Find all posts which were reacted to with a heart. For such posts output all columns from facebook_posts table.*/
+ 
 # Code 
 select distinct fp.* from facebook_posts fp
 inner join facebook_reactions as fr
