4732794c57cb69babf48c0bfe3de3aafd45a2d80
Shashikant Shukla <sks7523830807@gmail.com>
2026-04-25 18:02:50 +0530

    Update comment to clarify SQL query purpose

--- 2026-04-25_SQL_005.sql
+++ 2026-04-25_SQL_005.sql
@@ -1,5 +1,5 @@
 # Company Name : Glassdoor
-/* Compare each employee's salary with the average salary of the corresponding department.
+/* Question :  Compare each employee's salary with the average salary of the corresponding department.
    Output the department, first name, and salary of employees along with the average salary of that department.*/
 # Code 
 select department, first_name, salary, 
