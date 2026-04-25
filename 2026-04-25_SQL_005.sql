75ae4f08b40a1e71ca38333e17e306682b8ac33a
Shashikant Shukla <sks7523830807@gmail.com>
2026-04-25 17:55:23 +0530

    Create 2026-04-25_SQL_005.sql

---
+++ 2026-04-25_SQL_005.sql
@@ -0,0 +1,8 @@
+# Company Name : Glassdoor
+/* Compare each employee's salary with the average salary of the corresponding department.
+   Output the department, first name, and salary of employees along with the average salary of that department.*/
+# Code 
+select department, first_name, salary, 
+avg(salary) over(partition by department) 
+from employee
+
