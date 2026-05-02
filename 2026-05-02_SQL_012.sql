ac032e241c8c6abbc48f7fe805a2e8e287be966c
Shashikant Shukla <sks7523830807@gmail.com>
2026-05-02 23:16:30 +0530

    Create 2026-05-02_SQL_012.sql

---
+++ 2026-05-02_SQL_012.sql
@@ -0,0 +1,10 @@
+# Company Name : Spotify
+/* Question : Find songs that have ranked in the top position. Output the track name and the number of times it ranked at the top.
+  Sort your records by the number of times the song was in the top position in descending order.*/
+
+# Code
+select  trackname,count(*) as times_top1
+from spotify_worldwide_daily_song_ranking
+where position = 1
+group by trackname
+order by times_top1 desc
