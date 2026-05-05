22a3f240b4170f496989a0c2ed47d93524d98864
Shashikant Shukla <sks7523830807@gmail.com>
2026-05-05 23:42:09 +0530

    Create 2026-05-05_SQL_015.sql
    Query to find order details for customers Jill and Eva, ordered by total cost.

---
+++ 2026-05-05_SQL_015.sql
@@ -0,0 +1,10 @@
+/* Question : Find order details made by Jill and Eva.
+Consider the Jill and Eva as first names of customers.
+Output the order date, details and cost along with the first name.
+Order records based on the customer id in ascending order.*/
+# Code 
+select first_name,order_date,order_details,total_order_cost from customers as c
+join orders as o 
+on c.id= o.cust_id
+Where first_name  in ('Jill','Eva')
+order by total_order_cost desc
